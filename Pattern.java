49080722f44d1cd114fc4af3989286311a44d3ad
karthikeyan1705 <56017915+karthikeyan1705@users.noreply.github.com>
2019-10-03 12:37:34 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -1,15 +1,11 @@
-import java.util.Scanner;
 public class Pattern {
-	public static void main(String[] args) {
- Scanner sc=new Scanner(System.in);
- int a=sc.nextInt();
-		for(int i=0;i<=a;i++) {
-			for(int j=5;j>i;j--)
-			{
-				System.out.print(" *");
-			}
-			System.out.println();
-		}
-	}
 
+
+	public static void main(String args[]) {
+
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[3]);
+
+}
 }
